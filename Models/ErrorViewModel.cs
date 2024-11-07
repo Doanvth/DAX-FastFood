9498cda85a689f000f061216f05c34c11d4cbfaf
DoanVTH <doanvth.fpt@gmail.com>
2024-11-07 12:55:52 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DAX_FastFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
