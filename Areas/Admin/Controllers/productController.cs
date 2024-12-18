1dd732a6bb59bc8a6cf25d7b12a6e3f78a90e500
Quốc Anh Nguyễn <quocanhn773@gmail.com>
2024-12-19 01:17:16 +0700

    Areas Admin

---
+++ Areas/Admin/Controllers/productController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_FastFood.Areas.Admin.Controllers
+{
+    public class productController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -28,7 +28,11 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+   name: "areas",
+   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=}/{id?}");
 
 app.Run();
