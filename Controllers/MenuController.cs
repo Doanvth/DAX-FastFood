559934a70d51b55f2dabc066907a59c7f23eb6dd
Khangdh162 <155737062+Khangdh162@users.noreply.github.com>
2024-12-21 14:10:09 +0700

    FE-Product Display

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using DAX_FastFood.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_FastFood.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MenuController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.products.Select(p => new
+            {
+                p.ProductId,
+                p.ProductName,
+                p.Price,
+                p.Image
+            }).ToList();
+
+
+            return View(products);
+        }
+    }
+}
