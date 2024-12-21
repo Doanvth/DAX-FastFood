2c3f19603a8402712a7127bc19b91118eef7eb35
Khangdh162 <155737062+Khangdh162@users.noreply.github.com>
2024-12-21 15:22:16 +0700

    FE-Full_Display

---
+++ Controllers/PromotionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_FastFood.Controllers
+{
+    public class PromotionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
