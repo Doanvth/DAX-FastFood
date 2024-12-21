69303c8d467563cf584a97dab0ac20c1d7709edb
Duy Hạo Nguyễn <nguyenduyhao2021hg@gmail.com>
2024-12-21 17:01:43 +0700

    [DH]AddGiaoDien

---
+++ Controllers/CartController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_FastFood.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAX_FastFood.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Address()
+        {
+            return View();
+        }
+
+        public IActionResult Pay()
+        {
+            return View();
+        }
+
+
+    }
+}
